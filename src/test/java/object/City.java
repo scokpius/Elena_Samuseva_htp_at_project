5a6320a09ac389326bffbdc5ef48fc157f123c9c
scokpius <scokpius@gmail.com>
2020-05-22 22:58:27 +0300

    Additional classes for reading data from a json file

---
+++ src/test/java/object/City.java
@@ -0,0 +1,11 @@
+package object;
+
+public class City {
+    public String name;
+    public int date_in;
+    public int date_out;
+    public int adults;
+    public int children;
+    public int rooms;
+
+}
---
+++ src/test/java/object/DataCity.java
@@ -0,0 +1,9 @@
+package object;
+
+public class DataCity {
+    public City[] city;
+
+    public DataCity(City[] city) {
+        this.city = city;
+    }
+}
